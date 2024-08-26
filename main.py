c21e6aafa64660d246eb7514a8d649b8f8bbb054
Ahmed Hussein <wolktheline@yahoo.com>
2024-08-25 21:25:18 -0600

    Adding another pages.

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 from flask import Flask, render_template
 
-web_page = Flask("Weather")
+web_page = Flask(__name__)
 
 
 @web_page.route("/")
 def home():
-    return render_template("tutorial.html")
+    return render_template("home.html")
 
 
-@web_page.route("/about/")
-def about():
-    return render_template("about.html")
+@web_page.route("/api/v1/<station>/<date>")
+def about(station, date):
+    temperature = 23
+    return {"station": station,
+            "date": date,
+            "temperature": temperature}
 
 
-web_page.run()
+if __name__ == "__main__":
+    web_page.run(debug=True)
