1c87f0f594bd3b6f0ed5e3f49847140b5340a1f4
Ahmed Hussein <wolktheline@yahoo.com>
2024-08-28 23:40:47 -0600

    Adding another API to show the data for one year only.

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ web_page = Flask(__name__)
 
 variable = "Hello World"
 
-
 stations = pd.read_csv("data_small/stations.txt", skiprows=17)
 
 stations = stations[["STAID", "STANAME                                 "]]
@@ -22,10 +21,26 @@ def about(station, date):
     df = pd.read_csv(f"data_small/TG_STAID" + str(station).zfill(6) + ".txt", skiprows=20, parse_dates=["    DATE"])
     df["TG0"] = df['   TG'].mask(df["   TG"] == -9999, np.nan)
     temperature = df.loc[df["    DATE"] == date]["TG0"].squeeze() / 10
-    print(temperature)
     return {"station": station,
             "date": date,
             "temperature": temperature}
+
+
+@web_page.route("/api/v1/<station>")
+def station(station):
+    df = pd.read_csv(f"data_small/TG_STAID" + str(station).zfill(6) + ".txt", skiprows=20,
+                     parse_dates=["    DATE"]).squeeze()
+    station_dict = df.to_dict(orient="records")
+    return station_dict
+
+
+@web_page.route("/api/v1/annual/<station>/<year>")
+def yearly(station,year):
+    df = pd.read_csv(f"data_small/TG_STAID" + str(station).zfill(6) + ".txt", skiprows=20)
+    df["    DATE"] = df["    DATE"].astype(str)
+    result = df[df["    DATE"].str.startswith(str(year))]
+    result = result.to_dict(orient="records")
+    return result
 
 
 if __name__ == "__main__":
